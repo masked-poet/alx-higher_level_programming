21d78c10801d7ac8942ccbd177bf95740ecda845
abraham <abrahamikpoma@gmail.com>
2023-03-09 03:51:06 -0800

    0x02. Python - import & modules

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def magic_calculation(x, y):
+    from magic_calculation_102 import add, sub
+    if x < y:
+        z = add(x, y)
+        for i in range(4, 6):
+           z = add(z, i)
+        return (z)
+    else:
+        return (sub(x, y))
